4ca88104875bedbe973a52f4b1eb80e9baf21de6
SkyEngineerME <guymechatronic@gmail.com>
2023-08-08 23:14:03 +0700

    add: Search and footprint

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -3,28 +3,20 @@
 import { MovieRow } from "@/components/MovieRow";
 import { movieDB } from "@/libs/movieDB";
 
-export default function SearchResultPage() {
-  //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
-  // const processedSearchInput = ...
+export default function SearchResultPage({params}) {
+  const search = params.searchInput.replaceAll("%20"," ");
 
-  /*
-  tip2 : Use "includes" string method to check substring
-  Example : "ABC".includes("AB") -> return true
-
-  tip3 : To implement case insensitive searching, use "toLocaleLowerCase" string method
-  to convert movie title and searchInput to lower case 
-  const filteredMovies = movieDB.filter((movie) =>
-    you code here...
+  const TarMov = movieDB.filter((movie) => movie.title.toLocaleUpperCase().includes(search.toLocaleUpperCase())
   );
-  */
+  
 
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; ... &quot;
+        Searching &quot; {search} &quot;
       </p>
-      <p className="fw-bold fs-4 text-center">Found ... result(s)</p>
-      {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      <p className="fw-bold fs-4 text-center">Found {TarMov.length} result(s)</p>
+      {TarMov.map((movie, i) => (<MovieRow key={movie.id} id={movie.id} title={movie.title} detail={movie.detail} rating={movie.rating} number={i + 1} />)) }
     </div>
   );
 }
